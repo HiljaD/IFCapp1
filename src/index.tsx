d66a7ee225a6050f08c8757dbb11b97b1cd5f359
Jari <jarirapani.turunen@gmail.com>
2023-05-30 13:09:30 +0300

    Install firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { initializeApp } from "firebase/app";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4gXrIWwjuh5q6x4KrwyyuTteQnUU16zQ",
+  authDomain: "ifc-app1.firebaseapp.com",
+  projectId: "ifc-app1",
+  storageBucket: "ifc-app1.appspot.com",
+  messagingSenderId: "642852850964",
+  appId: "1:642852850964:web:bcdc1b50458a50e2c8c601"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
